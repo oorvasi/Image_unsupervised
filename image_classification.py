b404e5bf525f299b7e02bf95ee020014469287fd
oorvasi sooprayen <minie_oorvasi@hotmail.com>
2020-09-10 16:52:05 +0200

    commit

--- image_classification.py
+++ image_classification.py
@@ -1,5 +1,19 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+from PIL import Image
+
 
 st.title('Image Classification')
+
+img_file_buffer = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
+
+image = Image.open(img_file_buffer)
+img_array = np.array(image)
+
+if image is not None:
+    st.image(
+        image,
+        caption=f"You amazing image has shape {img_array.shape[0:2]}",
+        use_column_width=True,
+    )
