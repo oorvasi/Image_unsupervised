2bcfc9335d456eacbb97015fac6ad1b03a8b961b
oorvasi sooprayen <minie_oorvasi@hotmail.com>
2020-09-24 17:04:38 +0200

    change

--- image_classification.py
+++ image_classification.py
@@ -11,12 +11,13 @@ from sklearn.metrics import accuracy_score
 import cv2
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-# On s'assurer que les valeurs sont flottantes afin que nous puissions obtenir des points décimaux après la division
 x_train = x_train.astype('float')
 x_test = x_test.astype('float')
-# Normaliser les codes RVB en les divisant par la valeur RVB maximale.
+
+# Normaliser
 x_train = x_train/255.0
 x_test = x_test/255.0
+
 # Redimmension des données d'entrée
 X_train = x_train.reshape(len(x_train),-1)
 X_test = x_test.reshape(len(x_test),-1)
@@ -40,9 +41,10 @@ y_pred = np.random.rand(len(kmeans.labels_))
 for i in range(len(kmeans.labels_)):
     y_pred[i] = reference_labels[kmeans.labels_[i]]
 
-# Testdu modèle sur le Test set
+# Test du modèle
 kmeans = MiniBatchKMeans(n_clusters = 256)
 kmeans.fit(X_test)
+
 #calculate_metrics(kmeans,y_test)
 reference_labels = retrieve_info(kmeans.labels_,y_test)
 y_pred = np.random.rand(len(kmeans.labels_))
@@ -61,25 +63,27 @@ from PIL import Image
 import numpy as np
 
 img_file_buffer = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
+try:
+    image = Image.open(img_file_buffer)
+    img_array = np.array(image)
+    st.text(f"Votre image est prédite comme un :{img_array.shape}.")
 
-image = Image.open(img_file_buffer)
-img_array = np.array(image)
-st.text(f"Votre image est prédite comme un :{img_array.shape}.")
+    if image is not None:
+        st.image(
+            image,
+            caption=f"Your amazing image has shape {img_array.shape[0:2]}",
+            use_column_width=True
+        )
 
-if image is not None:
-    st.image(
-        image,
-        caption=f"Your amazing image has shape {img_array.shape[0:2]}",
-        use_column_width=True,
-    )
+    # RGB image is converted to Monochrome image
+    gray = cv2.cvtColor(img_array, cv2.COLOR_BGR2GRAY)
 
-# RGB image is converted to Monochrome image
-gray = cv2.cvtColor(img_array, cv2.COLOR_BGR2GRAY)
-
-size = (28, 28)
-image_resize = resize(gray,size,anti_aliasing=True)
-image_resize = image_resize.reshape(1, 28*28)
-predicted_cluster = kmeans.predict(image_resize)
-prediction = y_pred[[predicted_cluster]]
-# # Text
-st.text(f"Votre image est prédite comme un :{prediction}.")
+    size = (28, 28)
+    image_resize = resize(gray,size,anti_aliasing=True)
+    image_resize = image_resize.reshape(1, 28*28)
+    predicted_cluster = kmeans.predict(image_resize)
+    prediction = y_pred[[predicted_cluster]]
+    # # Text
+    st.text(f"Votre image est prédite comme un :{prediction}.")
+except:
+    print("")
